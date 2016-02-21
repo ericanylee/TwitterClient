41ca334a2c9de267438157153b3b0133d55fb2fd
Erica Lee <ericanylee@gmail.com>
2016-02-21 12:56:53 -0600

    initial commit

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Erica Lee on 2/16/16.
+//  Copyright © 2016 Erica Lee. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
---
+++ Twitter/twitter.m
@@ -0,0 +1,9 @@
+//
+//  twitter.m
+//  Twitter
+//
+//  Created by Erica Lee on 2/16/16.
+//  Copyright © 2016 Erica Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
