3c891ee41c70fee0ea842eddc51de409925804b0
Erica Lee <ericanylee@gmail.com>
2016-02-21 21:50:58 -0600

    current user saving

--- Twitter/User.swift
+++ Twitter/User.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class User: NSObject {
 
+    //these are called stored properties, space allocated for them
     var name: NSString? //optional : can potentially be blank
     var screenname: NSString?
     var profileUrl: NSURL?
     var tagline: NSString?
     
+    var dictionary: NSDictionary?
+    
     //deserialization code. takes dictionary and selectively populating the properties
     init(dictionary: NSDictionary){//initializer... when u create a user u initialize it. Constructor!
+        self.dictionary = dictionary
         name = dictionary["name"] as? String // attempt to cast it as a string
         screenname = dictionary["screen_name"] as? String
         let profileUrlString = dictionary["profile_image_url_https"] as? String // can be a nil
@@ -28,4 +32,31 @@ class User: NSObject {
         }
         tagline = dictionary["description"] as? String
     }
+    
+    static var _currentUser: User?
+    //computed property, needs getter
+    class var currentUser: User?{//may or may not exist
+        get{//runs when someone tries to access this property
+            let defaults = NSUserDefaults.standardUserDefaults()
+            let userData = defaults.objectForKey("currentUserData") as? NSData
+            if let userData = userData{
+                let dictionary = try! NSJSONSerialization.JSONObjectWithData(userData, options: []) as! NSDictionary
+                _currentUser = User(dictionary: dictionary)
+            }
+            return _currentUser
+        }
+
+        set (user){
+            let defaults = NSUserDefaults.standardUserDefaults()
+            if let user = user {
+                let data = try! NSJSONSerialization.dataWithJSONObject(user.dictionary!, options: []) //[] default options
+                defaults.setObject(data, forKey: "currentUserData")
+            } else {
+                defaults.setObject(nil, forKey: "currentUserData")
+            }
+            defaults.synchronize()
+        }
+        
+    }
+
 }
